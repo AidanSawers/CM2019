ed6d479952b499ea60864ebb4c31cf5faadb01bb
Aidan <aidansawers_1@hotnmail.com>
2019-07-08 10:56:53 +1000

    New file

---
+++ Rotation.py
@@ -0,0 +1,15 @@
+from microbit import *
+import neopixel
+from random import randint
+
+np = neopixel.NeoPixel(pin13, 12)
+
+while True:
+
+    for i in range(0, len(np)):
+        np[i] = (60, 0, 0)
+        for j in range(0, len(np)):
+            if not (i == j):
+                np[j] = (0, 0, 60)
+            np.show()
+            sleep(1500)
