b2af28c9b56850e865a8e7aef5e5553be2c7361f
Aidan <aidansawers_1@hotnmail.com>
2019-07-08 10:36:19 +1000

    Added neopixel

---
+++ Neopixel.py
@@ -0,0 +1,15 @@
+from microbit import *
+import neopixel
+
+np = neopixel.NeoPixel(pin13, 12)
+
+while True:
+
+    for pixel_id in range(0, len(np)):
+        red = randint(0, 60)
+        green = randint(0, 60)
+        blue = randint(0, 60)
+
+        np[pixel_id] = (red, green, blue)
+        np.show()
+        sleep(100)
